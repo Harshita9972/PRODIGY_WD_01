33e80ffcd78fcffe5fb048a30ccf77d30cfc3e8c
Harshita9972 <harshinaik3@gmail.com>
2026-05-09 19:24:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navbar");
+
+    if (window.scrollY > 60) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
